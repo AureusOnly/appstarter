759dd9fc6bd516c0e749405254816918935c6a1b
AureusOnly <159770929+AureusOnly@users.noreply.github.com>
2024-04-12 16:47:21 +0200

    login page style

---
+++ app/Views/admin_accueil.php
@@ -0,0 +1,8 @@
+<h2> Welcome Dear administrator</h2>
+
+
+    <div class="menu">
+        <ul>
+            <li class="logo">
+
+  
--- app/Views/login_form.php
+++ app/Views/login_form.php
@@ -1,11 +1,18 @@
 
 
-
-    <h1>Se connecter</h1>
-    <form method="POST" action="/login">
-        <label for="login"> matricule abonné / identifiant admin </label>
-        <input id="login" name="login" type="text"/>
-        <label for="password"> nom abonné / mot de passe admin </label>
-        <input id="password" name="password" type="password"/>
+    <div class="login">
+        <h1>Se connecter</h1>
+        <form method="POST" action="/login">
+            <div class="container">
+                <input required="" type="text" name="text" class="input">
+                <label class="label" for="login"> matricule abonné / identifiant admin </label>
+                <input id="login" name="login" type="text"/>
+            </div>
+            <div class="container">
+                <input required="" type="text" name="text" class="input">
+                <label class="label" for="password"> nom abonné / mot de passe admin </label>
+                <input id="password" name="password" type="password"/>
+            </div>
         <button>se connecter</button>
     </form>
+    </div>
