130a931881e88e50dc2026c981356e5288d30209
Ilyan PASCOLINI <pascoliniilyan@gmail.com>
2024-04-22 12:53:51 +0200

    Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-routes->get('/', 'Home::index' , ['filter' =>\App\Filters\AuthenticatedFilter::class]);
-routes->get('/home', 'Home::index',['filter' =>\App\Filters\AuthenticatedFilter::class]);
-routes->get("/abonnes","Abonne::index", ["filter"  => "isAdmin]):
-routes->get('/login', 'Connection::index');
-routes->post('/login', 'Connection::attemptLogin');
+$routes->get('/', 'Home::index' , ['filter' =>\App\Filters\AuthenticatedFilter::class]);
+$routes->get('/home', 'Home::index',['filter' =>\App\Filters\AuthenticatedFilter::class]);
+$routes->get("/abonnes","Abonne::index", ["filter"  => "isAdmin]):
+$routes->post("/abonnes/(:num)", "Abonne::updateAbonne", ["filter" => "isAdmin]);
+
+
+
+
+$routes->get('/login', 'Connection::index');
+$routes->post('/login', 'Connection::attemptLogin');
