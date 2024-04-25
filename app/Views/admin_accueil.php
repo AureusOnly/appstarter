062c73a449a55442a494a965548ce619c2adf862
AureusOnly <159770929+AureusOnly@users.noreply.github.com>
2024-04-25 10:51:36 +0200

    all changes

--- app/Views/admin_accueil.php
+++ app/Views/admin_accueil.php
@@ -1,3 +1,9 @@
 <h2> Welcome Dear administrator</h2>
 
-  
+<ul>
+    <li>Gestion des abonnés</li>
+    <li>Gestion des Livres</li>
+    <li>Gestion des exemplaires</li>
+    <li>Gestion des emprunts</li>
+    <li></li>
+</ul>
