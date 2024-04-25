603d1d4e76a1889d18d599fddea34706e063521c
AureusOnly <159770929+AureusOnly@users.noreply.github.com>
2024-04-25 09:51:00 +0200

    sidebar changes

---
+++ app/Views/templates/side_menu.php
@@ -0,0 +1,55 @@
+<NAV>
+        <div class="sidebar-header">
+            <a class="logo-wrapper">
+                <img src="" alt="logo">
+                <H2 class="hidden">MENU</H2>
+            </a>
+            <button class="toggle-btn">
+                <img src="" alt="expand button">
+            </button>
+        </div>
+
+        <div class="sidebar-links">
+            <a class="link active">
+                <img src="" alt="">
+                <span class="hidden">Menu</span>
+            </a>
+    
+            <a class="sidebar-links">
+                <img src="" alt="">
+                <span class="hidden">Gestion Abonnes</span>
+            </a>
+
+            <a class="sidebar-links">
+                <img src="" alt="">
+                <span class="hidden">Gestion Emprunts</span>
+            </a>
+
+            <a class="sidebar-links">
+                <img src="" alt="">
+                <span class="hidden">Gestion Livres</span>
+                <a class="sidebar-link-2">
+                    <img src="" alt="">
+                    <span class="hidden">Gestion Exemplaires</span>
+                </a>
+            </a>
+        </div>
+
+    <div class="sidebar-bottom">
+        <div class="sidebar-links">
+            <a class="link">
+                <img src="" alt="">
+                <span class="hidden"></span>
+            </a>
+        </div>
+
+        <div class="user-profile">
+            <div class="user-avatar">
+                <img src="" alt="">
+            </div>
+            <div class="user-details hidden">
+                <p class="username"></p>
+            </div>
+        </div>
+    </div>
+</NAV> 
