1dbe9208a340b26a61608e1db59057a39bfd2a4c
AureusOnly <159770929+AureusOnly@users.noreply.github.com>
2024-04-26 14:20:04 +0200

    ajout des pages

--- app/Views/admin_accueil.php
+++ app/Views/admin_accueil.php
@@ -1,9 +1,7 @@
-<h2> Welcome Dear administrator</h2>
 
 <ul>
-    <li>Gestion des abonnés</li>
-    <li>Gestion des Livres</li>
-    <li>Gestion des exemplaires</li>
-    <li>Gestion des emprunts</li>
-    <li></li>
+     <li><a href="https://github.com/AureusOnly/appstarter/blob/JA/app/Views/abonnes.php">Gestion des abonnes</a></li>
+     <li><a href="https://github.com/AureusOnly/appstarter/blob/062c73a449a55442a494a965548ce619c2adf862/app/Views/emprunts.php">Gestion des Emprunts</a></li>
+     <li><a href="https://github.com/AureusOnly/appstarter/blob/062c73a449a55442a494a965548ce619c2adf862/app/Views/livres.php">Gestion des Livres</a></li>
+     <li><a href="https://github.com/AureusOnly/appstarter/blob/062c73a449a55442a494a965548ce619c2adf862/app/Views/exemplaires.php">Gestion des exemplaires</a></li>
 </ul>
