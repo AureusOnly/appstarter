ebb5e86d5215922249637120855779bfa712b04c
AureusOnly <159770929+AureusOnly@users.noreply.github.com>
2024-04-29 09:03:45 +0200

    LAST COMMIT

--- app/Views/templates/side_menu.php
+++ app/Views/templates/side_menu.php
@@ -1,63 +1,50 @@
 <nav>
       <div class="sidebar-header">
-        <a class="logo-wrapper">
-          <img src="./assets/logo.svg" alt="Logo">
-          <h2 class="hidden">LOGO</h2>
-        </a>
         <button class="toggle-btn">
-          <img src="./assets/expand.svg" alt="expand button">
+          <img src="file:///C:/Projets/appstarter/public/assets/img/Nucleo+UI+Essential/Nucleo%20UI%20Essential/SVG/fill/chevron-right.svg" alt="expand button">
         </button>
       </div>
 
 
       <div class="sidebar-links">
         <a class="link active">
-          <img src="./assets/home.svg" alt="">
+          <img src="file:///C:/Projets/appstarter/public/assets/img/Nucleo+UI+Essential/Nucleo%20UI%20Essential/SVG/fill/house-2.svg" alt="home">
           <span class="hidden">Home</span>
         </a>
         <a href="abonnes" class="link">
-          <img src="./assets/projects.svg" alt="">
+          <img src="file:///C:/Projets/appstarter/public/assets/img/Nucleo+UI+Essential/Nucleo%20UI%20Essential/SVG/fill/progress-bar.svg" alt="Abonnes">
           <span class="hidden">Abonnes</span>
         </a>
         <a href="emprunts" class="link">
-          <img src="./assets/dashboard.svg" alt="">
+          <img src="" alt="Emprunts">
           <span class="hidden">Emprunts</span>
         </a>
         <a href="livres" class="link">
-          <img src="./assets/dashboard.svg" alt="">
+          <img src="file:///C:/Projets/appstarter/public/assets/img/Nucleo+UI+Essential/Nucleo%20UI%20Essential/SVG/fill/book-bookmark.svg" alt="Livres">
           <span class="hidden">Livres</span>
         </a>
         <a href="exemplaires" class="link">
-          <img src="./assets/dashboard.svg" alt="">
+          <img src="file:///C:/Projets/appstarter/public/assets/img/Nucleo+UI+Essential/Nucleo%20UI%20Essential/SVG/fill/files-2.svg" alt="Exemplaires">
           <span class="hidden">Exemplaires</span>
         </a>
         <a href="demandes" class="link">
-          <img src="./assets/dashboard.svg" alt="">
+          <img src="file:///C:/Projets/appstarter/public/assets/img/Nucleo+UI+Essential/Nucleo%20UI%20Essential/SVG/fill/layers-3.svg" alt="Demandes">
           <span class="hidden">Demandes</span>
         </a>
         <a href="retours" class="link">
-          <img src="./assets/dashboard.svg" alt="">
+          <img src="file:///C:/Projets/appstarter/public/assets/img/Nucleo+UI+Essential/Nucleo%20UI%20Essential/SVG/fill/progress-bar.svg" alt="Retours">
           <span class="hidden">Retours</span>
         </a>
-        </li>
-        <a class="link">
-          <img src="./assets/tasks.svg" alt="">
-          <span class="hidden">Tasks</span>
+        <a href="deconnexion" class="link">
+          <img src="file:///C:/Projets/appstarter/public/assets/img/Nucleo+UI+Essential/Nucleo%20UI%20Essential/SVG/fill/arrow-door-in.svg" alt="Deconnexion">
+          <span class="hidden">Deconnexion</span>
         </a>
+        </li>
       </div>
-
-
       <div class="sidebar-bottom">
-        <div class="sidebar-links">
-          <a class="link">
-            <img src="./assets/settings.svg" alt="">
-            <span class="hidden">Settings</span>
-          </a>
-        </div>
-
         <div class="user-profile">
           <div class="user-avatar">
-            <img src="./assets/profile-pic.jpg" alt="">
+            <img src="" alt="profile img">
           </div>
           <div class="user-details hidden">
             <p class="username">Jane Doe</p>
