465c6cc78b379dccd58555f5e5f3f5e8993069ac
Kylian LAURENT <kylian.laurent12@gmail.com>
2024-04-30 10:20:21 +0200

    demandes

---
+++ app/Controllers/liste_demandes.php
@@ -0,0 +1,13 @@
+namespace App\Controllers;
+
+class Demandes extends BaseController
+{
+    public function afficherListe()
+    {
+        // Récupérez toutes les demandes passées (par exemple, depuis une base de données)
+        $demandes = $this->demandeModel->getDemandesPassees(); 
+        
+        // Transmettez les demandes à la vue pour l'affichage
+        return view('liste_des_demandes', ['demandes' => $demandes]);
+    }
+}
