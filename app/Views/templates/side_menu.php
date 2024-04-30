522a256e9f39254822a705214f80fae736102565
AureusOnly <159770929+AureusOnly@users.noreply.github.com>
2024-05-01 01:48:21 +0200

    still trying to fixe this side menu

--- app/Views/templates/side_menu.php
+++ app/Views/templates/side_menu.php
@@ -1,48 +1,39 @@
-<script src="https://kit.fontawesome.com/b02458ab2c.js" crossorigin="anonymous"></script>
-
 <div class="side-menu">
-  <i class="fas fa-bars" id="toggleButton"></i>
+  <button id="toggleButton">Menu</button>
   <div class="menu-container">
     <ul id="menuList" style="display: none;">
       <li>
         <a href="abonnes">
-          <img src="abonnes-icon.png" alt="Abonnes">
           <span class="link-name">Abonnes</span>
         </a>
       </li>
       <li>
         <a href="livres">
-          <img src="livres-icon.png" alt="Livres">
           <span class="link-name">Livres</span>
         </a>
       </li>
       <li>
         <a href="exemplaires">
-          <img src="abonnes-icon.png" alt="Exemplaires">
           <span class="link-name">Exemplaires</span>
         </a>
       </li>
       <li>
         <a href="emprunts">
-          <img src="abonnes-icon.png" alt="Emprunts">
           <span class="link-name">Emprunts</span>
         </a>
       </li>
       <li>
         <a href="demandes">
-          <img src="abonnes-icon.png" alt="Demandes">
           <span class="link-name">Demandes</span>
         </a>
       </li>
       <li>
         <a href="retours">
-          <img src="abonnes-icon.png" alt="Retours">
           <span class="link-name">Retours</span>
         </a>
       </li>
       <li>
         <a href="deconnexion">
-          <img src="abonnes-icon.png" alt="Déconnexion">
           <span class="link-name">Déconnexion</span>
         </a>
       </li>
